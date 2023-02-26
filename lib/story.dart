967bc0ed700f2911afdd96b5fe83f8e758a066f6
Lokesh S <lokeshlucky13579@gmail.com>
2023-02-26 22:52:47 +0530

    challenge completed

--- lib/story.dart
+++ lib/story.dart
@@ -1,8 +1,8 @@
-
 class Story {
   late String storyTitle;
   late String choice1;
   late String choice2;
 
-  Story(this.storyTitle, this.choice1, this.choice2);
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
